43e52bb4a58960f3bc5a9dda9526154741331d29
Kazeem <97337763+thedoskit@users.noreply.github.com>
2025-05-26 16:14:09 +0100

    Kazeem

---
+++ Models/Supplier.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace inventoryApp.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [StringLength(100)]
+        public string? ContactPerson { get; set; }
+
+        [EmailAddress]
+        [StringLength(100)]
+        public string? Email { get; set; }
+
+        [Phone]
+        [StringLength(20)]
+        public string? PhoneNumber { get; set; }
+
+        [StringLength(200)]
+        public string? Address { get; set; }
+
+    }
+}
